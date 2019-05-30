a19550ba87c258c365cddd55c17b4cde725d1701
SAKATA Sinji <sinji@sakasin.net>
2019-05-31 01:08:38 +0900

    Operate with priority

--- 9cc.c
+++ 9cc.c
@@ -17,6 +17,7 @@ typedef struct {
 
 char *user_input;
 
+int pos;
 Token tokens[100];
 
 void error(char *fmt, ...) {
@@ -46,7 +47,8 @@ void tokenize() {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' ||
+        *p == ')') {
       tokens[i].ty = *p;
       tokens[i].input = p;
       i++;
@@ -69,6 +71,116 @@ void tokenize() {
   tokens[i].input = p;
 }
 
+// node type
+enum {
+  ND_NUM = 256,
+};
+
+typedef struct Node {
+  int ty;
+  struct Node *lhs;
+  struct Node *rhs;
+  int val;
+} Node;
+
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+int consume(int ty) {
+  if (tokens[pos].ty != ty) {
+    return 0;
+  }
+  pos++;
+  return 1;
+}
+
+Node *expr();
+Node *mul();
+Node *term();
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+')) {
+      node = new_node('+', node, mul());
+    } else if (consume('-')) {
+      node = new_node('-', node, mul());
+    } else {
+      return node;
+    }
+  }
+}
+
+Node *mul() {
+  Node *node = term();
+
+  for (;;) {
+    if (consume('*')) {
+      node = new_node('*', node, term());
+    } else if (consume('/')) {
+      node = new_node('/', node, term());
+    } else {
+      return node;
+    }
+  }
+}
+
+Node *term() {
+  if (consume('(')) {
+    Node *node = expr();
+    if (!consume(')')) {
+      error_at(tokens[pos].input, "Expect ')'");
+    }
+    return node;
+  }
+
+  if (tokens[pos].ty == TK_NUM) {
+    return new_node_num(tokens[pos++].val);
+  }
+
+  error_at(tokens[pos].input, "Expect '(' or number");
+  return NULL; // Can not reach here
+}
+
+void gen(Node *node) {
+  if (node->ty == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  if (node->ty == '+') {
+    printf("  add rax, rdi\n");
+  } else if (node->ty == '-') {
+    printf("  sub rax, rdi\n");
+  } else if (node->ty == '*') {
+    printf("  imul rdi\n");
+  } else if (node->ty == '/') {
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "Wrong argument number");
@@ -78,40 +190,15 @@ int main(int argc, char **argv) {
   user_input = argv[1];
   tokenize();
 
+  Node *node = expr();
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  if (tokens[0].ty != TK_NUM) {
-    error_at(tokens[0].input, "Not number");
-  }
-  printf("  mov rax, %d\n", tokens[0].val);
+  gen(node);
 
-  int i = 1;
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error_at(tokens[i].input, "Not number");
-      }
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error_at(tokens[i].input, "Not number");
-      }
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    error_at(tokens[i].input, "Unexpected token");
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
